fd7c1033277fada8470c3ea9f90bdeac64612523
Takagi Tasuku <tasuren@outlook.jp>
2025-11-29 12:18:41 +0900

    feat: add list_displays example

---
+++ examples/list_displays.rs
@@ -0,0 +1,15 @@
+use display_config::get_displays;
+
+fn main() {
+    let displays = get_displays().expect("Failed to get displays");
+
+    for display in displays {
+        println!("Display ID: {:?}", display.id());
+
+        println!("  Origin: {:?}", display.origin());
+        println!("  Size: {:?}", display.size());
+        println!("  Is Mirrored: {}", display.is_mirrored());
+
+        println!("");
+    }
+}
